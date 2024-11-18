34058c08f11a08ed7f65fb51ce48d3cee7851cad
masktv <masktvgit@gmail.com>
2024-11-18 11:27:58 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-# CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-# FLUSH PRIVILEGES;
+ #  For both i will have to add credential in jenkins and  pass them through dpcker build cmd with using env, so can value of this [ ${MYSQL_USER}, ${MYSQL_PASSWORD}, ${MYSQL_USER} ] gives from jenkins env 
+    # CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    # CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    # GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    # FLUSH PRIVILEGES;
+
