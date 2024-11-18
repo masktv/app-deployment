f9f4dfa6d31c4fb9235d88dd9f5964d17379068b
masktv <masktvgit@gmail.com>
2024-11-18 11:30:22 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
- #  For both i will have to add credential in jenkins and  pass them through dpcker build cmd with using env, so can value of this [ ${MYSQL_USER}, ${MYSQL_PASSWORD}, ${MYSQL_USER} ] gives from jenkins env 
+ #  For both i will have to add credential in jenkins and  pass them through docker build cmd, so can value of this [ ${MYSQL_USER}, ${MYSQL_PASSWORD}, ${MYSQL_USER} ] gives from jenkins env 
     # CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
     # CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     # GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
